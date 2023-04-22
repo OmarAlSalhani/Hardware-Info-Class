f688baa196d84676fdb3613c22762db2b2ab9459
omarsalhani <omarsalhanir@gmail.com>
2023-04-22 18:02:17 +0200

    init

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System.Management;
+
+namespace Salhani.HardwareInfo
+{
+    public class GetInfo
+    {
+        public string GetCpuSerial()
+        {
+            string Variable = null;
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+            foreach (ManagementObject mo in moc)
+            {
+                Variable = mo.Properties["processorID"].Value.ToString();
+                
+                break;
+            }
+            return Variable;
+        }
+        public string GetHardDriveSerial()
+        {
+            string Variable = null;
+            ManagementObjectSearcher searcher = new
+            ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                if(wmi_HD.Properties.Count>1)
+                Variable = wmi_HD["SerialNumber"].ToString();
+                break;
+            }
+            return Variable;
+        }
+    }
+}
